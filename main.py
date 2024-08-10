6d6294910db08a98f77c275e9a98e319f154ac86
maxpanev <maxpanev170100@gmail.com>
2024-08-10 14:57:00 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init() # Чтобы запустить Pygame
 
@@ -8,7 +9,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Создаем
 
 
 pygame.display.set_caption("Игра Тир") # Задаем заголовок окна
-icon = pygame.image.load("icon.png") # Загружаем иконку приложения
+icon = pygame.image.load("img/42648.jpg") # Загружаем иконку приложения
+pygame.display.set_icon(icon) # Устанавливаем иконку приложения
+
+target_image = pygame.image.load("img/target.png") # Загружаем изображение цели
+target_width, target_height = 80, 80 # Получаем размеры изображения цели
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width) # Случайное положение цели по горизонтали
+target_y = random.randint(0, SCREEN_HEIGHT - target_height) # Случайное положение цели по вертикали
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
 
 
 running = True
