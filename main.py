d8819db0a153dca7134a35d13180600a2522f389
maxpanev <maxpanev170100@gmail.com>
2024-08-10 13:39:01 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 
 pygame.init() # Чтобы запустить Pygame
 
+SCREEN_WIDTH = 800 # Задаем переменные с размерами экрана
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Создаем экран
+
+
+pygame.display.set_caption("Игра Тир") # Задаем заголовок окна
+icon = pygame.image.load("icon.png") # Загружаем иконку приложения
+
+
 running = True
 while running:
     pass # Здесь будет основной цикл Pygame
